5c84d680d2c069132d6913647b424cd1bb21d734
Pham Anh Tuan <dev@patchybean.com>
2025-12-19 22:29:57 +0700

    chore: update neatcli to v0.3.0

--- Formula/neatcli.rb
+++ Formula/neatcli.rb
@@ -1,22 +1,22 @@
 class Neatcli < Formula
   desc "A smart CLI tool to organize and clean up messy directories"
   homepage "https://github.com/patchybean/neatcli"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/patchybean/neatcli/releases/download/v#{version}/neatcli-aarch64-apple-darwin.tar.gz"
-      sha256 "04582f3c3b6e45968a814d192666e9571724e03edc5176dd5cf0b48507967ea0"
+      sha256 "e5b74f00fa643ed825268b1cb2eb4c0d9ff2e3e134d15e7efeb5f15292f3720c"
     else
       url "https://github.com/patchybean/neatcli/releases/download/v#{version}/neatcli-x86_64-apple-darwin.tar.gz"
-      sha256 "d7569f038d246166be386b3d3a9b1bf166aeafd9222409fdb068ff535ebb70da"
+      sha256 "aa5addbef0196ba9175aeb17f667da78869fd23340c4d42d107d15052ae65a3d"
     end
   end
 
   on_linux do
     url "https://github.com/patchybean/neatcli/releases/download/v#{version}/neatcli-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "f2a1404857dbeb96ceb42bdeaad66c7c4dea9bae86b9b9044fe4b73475ab3624"
+    sha256 "24e253bf7ff3a3c8b074db54f8a16458a6c00f5f47357a069a37d11dda4ebfe5"
   end
 
   def install
